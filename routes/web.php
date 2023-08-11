6d1cc6a1d18f933dd20f57b5621cb51b209a9558
Syaim Smp <syaimsmp@gmail.com>
2023-08-11 22:24:22 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [App\Http\Controllers\WorkspaceController::class, 'index']);
 
 Route::resource('workspace', WorkspaceController::class);
+
+Route::get('test', [TaskController::class, 'tester']);
 Route::resource('task', TaskController::class);
 // Route::get('workspace/{workspace_id}', [WorkspaceController::class, 'show'])->name('show-workspace');
 Route::get('update_task/{task}', [TaskController::class, 'updateTask'])->name('task.update-task');
